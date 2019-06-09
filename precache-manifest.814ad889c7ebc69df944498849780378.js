e2195c54020793eca0ebe200768d3a6c96e19ce3
Milton Li <milton@aeglepalette.com>
2019-06-09 14:44:46 -0400

    Under construction website

---
+++ precache-manifest.814ad889c7ebc69df944498849780378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a252d91f905e2db3f30e25d40079f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47a5065b1bd427607ee5",
+    "url": "/static/css/main.b95eaddd.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "47a5065b1bd427607ee5",
+    "url": "/static/js/main.91dd5b88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
